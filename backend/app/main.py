c6a3bb747267f12e367115f7c2c51117b2be1bab
OG-Jons <jonas@marschall.pro>
2025-02-17 20:41:53 +0100

    feat: create sqlite database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,29 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlmodel import SQLModel
 
 from app.api.main import api_router
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+    SQLModel.metadata.drop_all(engine)
+
 app = FastAPI(
     title="Planz",
+    lifespan=lifespan
 )
+
+sqlite_file_name = "plants.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 app.include_router(api_router, prefix="/api")
