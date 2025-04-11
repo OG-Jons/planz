510116044d86be6b225b5e7a440a95280d348cb5
Jonas Marschall <jonas.marschall@swisscom.com>
2025-04-11 21:32:30 +0200

    feat: add soil moisture percentage calculation in plant stats

--- backend/app/api/routes/stats.py
+++ backend/app/api/routes/stats.py
@@ -1,4 +1,4 @@
-import random
+import random, math
 
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi_utilities import repeat_every
@@ -9,12 +9,19 @@ from app.core.db import get_session
 
 router = APIRouter()
 
+max_moisture = 520
+
+def calc_soil_percentage(soil_value: float):
+    # TODO: Verify true max-min values
+    return 100 / max_moisture * soil_value
+
 @router.post("/plants/{plant_id}/stats", response_model=Stat)
 def create_stat(
         plant_id: int,
         stat: StatCreate,
         session: Session = Depends(get_session)
 ):
+    stat.soil_moisture_score = calc_soil_percentage(stat.soil_moisture_score)
     plant = session.get(Plant, plant_id)
     if not plant:
         raise HTTPException(status_code=404, detail="Plant not found")
--- backend/cron-create-stats.py
+++ backend/cron-create-stats.py
@@ -1,20 +1,25 @@
 import random
 
 import requests
-
 data1 = {
     "humidity_score": random.randint(50, 60),
     "sunlight_score": random.randint(20, 60),
+    "temperature_score": random.randint(20, 30),
+    "soil_moisture_score": random.randint(430, 520),
 }
 data2 = {
     "humidity_score": random.randint(20, 60),
     "sunlight_score": random.randint(50, 60),
+    "temperature_score": random.randint(20, 30),
+    "soil_moisture_score": random.randint(260, 430),
 }
 data3 = {
     "humidity_score": random.randint(40, 45),
     "sunlight_score": random.randint(55, 60),
+    "temperature_score": random.randint(20, 30),
+    "soil_moisture_score": random.randint(0, 60),
 }
 
 r1 = requests.post("http://localhost:8000/api/plants/1/stats", json=data1)
 r2 = requests.post("http://localhost:8000/api/plants/2/stats", json=data2)
-r3 = requests.post("http://localhost:8000/api/plants/3/stats", json=data3)
+# r3 = requests.post("http://localhost:8000/api/plants/3/stats", json=data3)
